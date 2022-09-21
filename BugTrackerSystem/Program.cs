0e21c9dca940fe1675bc29621d9a489a402e21d2
Anıl Karaşah <anilkarasah@hotmail.com>
2022-09-21 12:14:15 +0300

    Update on Program.cs

--- BugTrackerSystem/Program.cs
+++ BugTrackerSystem/Program.cs
@@ -8,15 +8,15 @@ global using BugTrackerAPI.Services;
 using System.Text.Json.Serialization;
 
 var envPort = Environment.GetEnvironmentVariable("PORT");
-var port = !string.IsNullOrWhiteSpace(envPort) ? envPort : "8001";
+var port = string.IsNullOrWhiteSpace(envPort) ? 8001 : int.Parse(envPort);
 
 var builder = WebApplication.CreateBuilder(args);
 {
 	builder.WebHost.UseKestrel(options => {
 		options.ListenAnyIP(8000);
-		options.ListenAnyIP(int.Parse(port), listen => listen.UseHttps());
+		options.ListenAnyIP(port, listen => listen.UseHttps());
 	});
-	
+
 	builder.Services.AddControllers().AddJsonOptions(options =>
 		options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
 	);
