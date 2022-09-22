8f8073a81a042fd4900d03a11fbceae3800abbd8
Anıl Karaşah <anilkarasah@hotmail.com>
2022-09-22 15:51:31 +0300

    Retrying :(

--- BugTrackerSystem/Program.cs
+++ BugTrackerSystem/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 {
 	builder.WebHost.UseKestrel(options => {
 		options.ListenAnyIP(8000);
-		options.ListenAnyIP(port, listen => listen.UseHttps());
+		options.Listen(System.Net.IPAddress.Parse("0.0.0.0"), port, listen => listen.UseHttps());
 	});
 
 	builder.Services.AddControllers().AddJsonOptions(options =>
