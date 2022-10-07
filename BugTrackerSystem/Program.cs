e93ac39031d3b5c4345ff8af13c681a5e211573a
Anıl Karaşah <anilkarasah@hotmail.com>
2022-10-07 18:36:31 +0300

    Still working on Railway.app configuration

--- BugTrackerSystem/Program.cs
+++ BugTrackerSystem/Program.cs
@@ -6,19 +6,27 @@ global using BugTrackerAPI.Data;
 global using BugTrackerAPI.Services;
 
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var envPort = Environment.GetEnvironmentVariable("PORT");
 var port = string.IsNullOrWhiteSpace(envPort) ? 8001 : int.Parse(envPort);
 
 var builder = WebApplication.CreateBuilder(args);
 {
-	builder.WebHost.UseKestrel(options => 
-		options.Listen(System.Net.IPAddress.Parse("0.0.0.0"), port)
-	);
+	builder.WebHost.UseKestrel(options => {
+		options.Listen(System.Net.IPAddress.Parse("0.0.0.0"), port);
+		options.ListenLocalhost(8000);
+	});
 
 	builder.Services.AddControllers().AddJsonOptions(options =>
 		options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
 	);
+	
+	builder.Services.Configure<ForwardedHeadersOptions>(options =>
+	{
+		options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+		options.ForwardedProtoHeaderName = "x-forwarded-proto";
+	});
 
 	// Integrate services from DependencyInjection class
 	builder.Services.AddServices(builder.Configuration);
@@ -26,6 +34,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+	app.UseForwardedHeaders();
 	app.UseCors();
 	app.UseExceptionHandler("/error");
 	app.UseAuthentication();
